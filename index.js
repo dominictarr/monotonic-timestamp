f0e540eadab3501fccc7640e5ff9b44068734960
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-31 23:35:49 +0100

    sneak past float problems...

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 var _last = 0
 var _count = 1
+var adjusted = 0
+var _adjusted = 0
 
 module.exports =
 function timestamp() {
@@ -14,15 +16,30 @@ function timestamp() {
   This function guarantees unique but maybe inaccurate results
   on each call.
   **/
+  //uncomment this wen
   var time = Date.now()
-  var adjusted = time
-  // If time returned is same as in last call, adjust it by
-  // adding 1 / 1000-th of the number in counter. Also counter
-  // is incremented so that next call get's adjusted properly.
-  if (_last === time) adjusted += _count++ / (_count + 999)
+  //time = ~~ (time / 1000) 
+  //^^^uncomment when testing...
+
+  /**
+  If time returned is same as in last call, adjust it by
+  adding a number based on the counter. 
+  Counter is incremented so that next call get's adjusted properly.
+  Because floats have restricted precision, 
+  may need to step past some values...
+  **/
+  if (_last === time)  {
+    do {
+      adjusted = time + (_count++ / (_count + 999))
+    } while (adjusted === _adjusted)
+    _adjusted = adjusted
+  }
   // If last time was different reset timer back to `1`.
-  else _count = 1
-  
+  else {
+    _count = 1
+    adjusted = time
+  }
+  _adjusted = adjusted
   _last = time
   return adjusted
 }
