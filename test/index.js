68b8e75aaef9c89cf134507ce549f78f00d958e7
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-31 21:07:08 +0100

    more reporting in test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
 var ts = require('..')
 var assert = require('assert')
-
-var l = 1000000
+var max = 1000000
+var l = max
 var prev = 0
 //assert that timestamps are strictly increasing!
 while (l--) {
   var t = ts()
   if(prev >= t) {
-    console.log('collision', prev, t)
+    console.log('collision', prev, t, 'after', max - l)
     assert(prev < t)
   }
   prev = t
